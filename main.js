61b0c55882666f8b8b91b2d94fbeb00a565728fa
Kush Patel <Kush@Kushs-MacBook-Pro-2.local>
2021-08-01 16:55:50 -0400

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// your code here
+var button = document.getElementsByTagName('button')[0];
+
+button.addEventListener('click',function(){
+  var nameInput = document.getElementById('name').value;
+  var messageInput = document.getElementById('message').value;
+
+  var nameP = document.createElement('p');
+  nameP.innerHTML = 'Posted By: <strong>' + nameInput + '</strong>';
+  nameP.style.borderBottom = '1px solid #eee';
+  nameP.style.paddingBottom = '20px';
+
+  var messageP = document.createElement('p');
+  messageP.innerHTML = messageInput;
+
+  document.getElementsByClassName('posts')[0].append(messageP);
+  document.getElementsByClassName('posts')[0].append(nameP);
+
+  document.getElementsByClassName('form-control')[0].value = '';
+  document.getElementsByClassName('form-control')[1].value = '';
+});
